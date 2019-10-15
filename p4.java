27cd5c2a24fe1dd86fa61041da68dbea578a9c62
TakamichiNomura <hs19980101@yhoo.co.jp>
2019-10-15 21:56:48 +0900

    p4를 만들었다.

--- p4.java
+++ p4.java
@@ -10,42 +10,54 @@ public class p4 extends JFrame
 {
     public p4(){
         setTitle("Ten Color Buttons Frame");
-        setSize(300,200);
+        setSize(100,200);
         setVisible(true);
         
-        JPanel j = new JPanel();
-        j.setLayout(new BorderLayout());
-        JButton zero = new JButton("0");
-        zero.setBackground(Color.RED);
-        j.add(zero);
-        JButton one = new JButton("1");
-        one.setBackground(Color.ORANGE);
-        j.add(one);
-        JButton two = new JButton("2");
-        two.setBackground(Color.YELLOW);
-        j.add(two);
-        JButton three = new JButton("3");
-        three.setBackground(Color.GREEN);
-        j.add(three);
-        JButton four = new JButton("4");
-        four.setBackground(Color.CYAN);
-        j.add(four);
-        JButton five = new JButton("5");
-        four.setBackground(Color.BLUE);
-        j.add(five);
-        JButton six = new JButton("6");
-        four.setBackground(Color.magenta);
-        j.add(six);
-        JButton seven = new JButton("7");
-        four.setBackground(Color.BLACK);
-        j.add(seven);
-        JButton eight = new JButton("8");
-        four.setBackground(Color.PINK);
-        j.add(eight);
-        JButton nine = new JButton("9");
-        four.setBackground(Color.GRAY);
-        j.add(nine);
-        this.add(j);
+        String array[] = {"0","1","2","3","4","5","6","7","8","9"};
+        Color color[] = {Color.RED,Color.ORANGE,Color.YELLOW,Color.GREEN,
+                         Color.CYAN,Color.BLUE,Color.magenta,Color.BLACK,
+                         Color.PINK,Color.GRAY};
+                          
+        this.setLayout(new GridLayout(1,10));
+        JButton button;
+        for(int i = 0;i<10;i++){
+            button = (new JButton(array[i]));
+            button.setBackground(color[i]);
+            this.add(button);
+        }
+        
+        // this.setLayout(new GridLayout(1,10));
+        
+        // JButton zero = new JButton("0");
+        // zero.setBackground(Color.RED);
+        // this.add(zero);
+        // JButton one = new JButton("1");
+        // one.setBackground(Color.ORANGE);
+        // this.add(one);
+        // JButton two = new JButton("2");
+        // two.setBackground(Color.YELLOW);
+        // this.add(two);
+        // JButton three = new JButton("3");
+        // three.setBackground(Color.GREEN);
+        // this.add(three);
+        // JButton four = new JButton("4");
+        // four.setBackground(Color.CYAN);
+        // this.add(four);
+        // JButton five = new JButton("5");
+        // five.setBackground(Color.BLUE);
+        // this.add(five);
+        // JButton six = new JButton("6");
+        // six.setBackground(Color.magenta);
+        // this.add(six);
+        // JButton seven = new JButton("7");
+        // seven.setBackground(Color.BLACK);
+        // this.add(seven);
+        // JButton eight = new JButton("8");
+        // eight.setBackground(Color.PINK);
+        // this.add(eight);
+        // JButton nine = new JButton("9");
+        // nine.setBackground(Color.GRAY);
+        // this.add(nine);
     }
     
     public static void main(String[] args){
